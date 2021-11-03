62d1ae129e2d622b4892cb14e381ebf235d490ab
Paul Schweng <paul.schweng@siemens.com>
2021-11-03 17:10:23 +0100

    navbar almost done (css is shit tho)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
+import {SearchComponent} from "./components/search/search.component";
+import {RatingsComponent} from "./components/ratings/ratings.component";
+import {ProfileComponent} from "./components/profile/profile.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'ratings', component: RatingsComponent},
+  {path: 'profile', component: ProfileComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
