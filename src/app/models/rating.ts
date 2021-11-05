fbfe9a43d489ea30c16d7abf43708cba3d8ed542
Paul Schweng <paul.schweng@siemens.com>
2021-11-05 01:29:02 +0100

    create models in UI [UU-150]

---
+++ src/app/models/rating.ts
@@ -0,0 +1,14 @@
+import {User} from "./user";
+import {Product} from "./product";
+import {Vote} from "./vote";
+
+export interface Rating {
+  id: string,
+  user: User,
+  stars: number,
+  description?: string,
+  timestamp: string,
+  image?: string | File,
+  product: Product,
+  votes: number | Vote[]
+}
