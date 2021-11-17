b80683838ba38929bcc692adad9563fa153156a3
Paul Schweng <paul.schweng@siemens.com>
2021-11-17 23:08:39 +0100

    added label model

---
+++ src/app/models/label.ts
@@ -0,0 +1,8 @@
+export interface Label {
+  en: string,
+  de: string,
+  icon: string
+}
+
+// switch between 'en' and 'de' like this:
+// https://stackoverflow.com/questions/47825728/typescript-dynamically-assign-a-part-of-variable-name
