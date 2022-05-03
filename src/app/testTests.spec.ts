b3fbed42acf94144c974fb94565b8ee8a65b3fa8
2002chris <526917c>
2022-05-03 21:08:19 +0200

    add test for tests

---
+++ src/app/testTests.spec.ts
@@ -0,0 +1,12 @@
+import { async, ComponentFixture, TestBed, inject, tick, fakeAsync} from "@angular/core/testing";
+
+import { DebugElement } from "@angular/core";
+import { By } from '@angular/platform-browser'
+
+describe('TestTest', () => {
+
+})
+
+it('should succeed', () =>{
+  expect(1).toBe(1);
+})
