25e054028dbf70957a4949e10f4e8b8ae1d195ea
2002chris <christian.geier02@t-online.de>
2022-06-22 15:49:02 +0200

    test commit

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
+        // you can add configuration options for Jasmine
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
